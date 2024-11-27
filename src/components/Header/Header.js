b47db5b6feb169621b66c11332dc526bf72ffbd3
Eric Park <me@ericswpark.com>
2024-11-27 12:52:54 -0500

    Tweak values for exercise 5

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -61,7 +61,6 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
   overflow-y: auto;
 
@@ -80,9 +79,9 @@ const MainHeader = styled.div`
 const DesktopNav = styled.nav`
   display: flex;
   gap: clamp(
-    1.1rem,
-    7vw - 2.75rem,
-    3rem
+    1rem,
+    9.2vw - 4.5rem,
+    3.5rem
   );
   margin: 0px 48px;
 
