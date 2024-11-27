dfe63f5130b536cda46c0bafad5e5bbcede6d92c
Eric Park <me@ericswpark.com>
2024-11-27 12:46:55 -0500

    Try Exercise 5

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -63,6 +63,7 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow-y: auto;
 
   @media ${QUERIES.tabletAndDown} {
     border-top: 4px solid ${COLORS.gray[900]};
@@ -78,7 +79,11 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(
+    1.1rem,
+    7vw - 2.75rem,
+    3rem
+  );
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndDown} {
